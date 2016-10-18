e4d6c7c8740bbe8ba392a92a191ff8964be7b0a1
Zac Colley <hi@zaccolley.com>
2016-10-18 23:08:35 +0100

    Show logo on all main pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -8,7 +8,7 @@ export default class Header extends Component {
     return (
       <header class={`${style.header} ${hasHeaderImage ? style.headerHasHeaderImage : ''}`}>
         <Link href={`/`}>
-          {currentUrl === '/' ? (
+          {currentUrl === '/' || currentUrl === '/upcoming' || currentUrl === '/settings'  ? (
             <h1 class={style.title}>Songkick</h1>
           ) : (
             <span class={style.back}>Back</span>
