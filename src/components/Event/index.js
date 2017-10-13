8a1906b586cd2ca55078a2a3f92ed0c55ef708ac
Zac Colley <hi@zaccolley.com>
2017-10-14 00:37:43 +0100

    Add Dice button

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -23,6 +23,7 @@ export default class Event extends Component {
   }
 
   componentWillMount() {
+    this.constructDiceUri();
     this.isShareAvailable();
   }
 
@@ -38,6 +39,33 @@ export default class Event extends Component {
     });
   }
 
+  constructDiceUri() {
+    const { events, id } = this.props;
+    const event = events.find(event => event.id === +id);
+
+    if (!event.performances[0].name || !event.place.name) {
+      return;
+    }
+
+    fetch(`https://dice.fm/_data/search/events;query=${event.performances[0].name} ${event.place.name}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.length === 0) {
+          return;
+        }
+
+        const diceUriPart = data[0].perm_name;
+
+        if (!diceUriPart) {
+          return;
+        }
+
+        const diceUri = `https://dice.fm/event/${diceUriPart}`;
+
+        this.setState({ diceUri });
+      });
+  }
+
   constructCityMapperUri() {
     const { events, id } = this.props;
     const event = events.find(event => event.id === +id);
@@ -51,10 +79,9 @@ export default class Event extends Component {
       event.place.name,
       event.place.city,
       event.place.country
-    ].filter(item => typeof item !== 'undefined').join(',')
+    ].filter(item => typeof item !== 'undefined').join(',');
 
     let citymapperUri = `https://citymapper.com/directions?endcoord=${event.place.lat},${event.place.lon}&endname=${event.place.name}&endaddress=${endAddress}`;
-    console.log(citymapperUri)
 
     if (lat && lon) {
       citymapperUri += `&startcoord=${lat},${lon}`;
@@ -150,7 +177,7 @@ export default class Event extends Component {
 
   render() {
     const { events, id } = this.props;
-    const { citymapperUri, travelTime, shareButtonVisible } = this.state;
+    const { diceUri, citymapperUri, travelTime, shareButtonVisible } = this.state;
 
     const event = events.find(event => event.id === +id);
 
@@ -177,9 +204,14 @@ export default class Event extends Component {
 
         <section>
           <h4><Icon name="shoppingCart" /> Tickets</h4>
-          <a class={`${style.button} ${style.buttonPrimary}`} href={event.uri} target="_blank">
-            Buy tickets! <Icon name="external" style={{ marginLeft: '0.25em', float: 'right' }} />
+          <a class={`${style.button} ${style.buttonSongkick}`} href={event.uri} target="_blank">
+            Songkick
           </a>
+          {diceUri && (
+          <a class={`${style.button} ${style.buttonDice}`} href={diceUri} target="_blank">
+            Dice
+          </a>
+          )}
         </section>
 
         <section>
